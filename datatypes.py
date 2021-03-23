8d30b2caf7b87679c6023a407c30a045c16a883e
billkd24 <nyerere.akide@strathmore.edu>
2021-03-23 09:51:03 +0300

    class excercise

---
+++ datatypes.py
@@ -0,0 +1,13 @@
+x = 3
+y = 3.5
+z = "Hello"
+
+a = True
+b = False
+c = None
+
+print (type(x))
+print (type(y))
+print (type(z))
+print (type(a))
+print (type(c))
---
+++ variables.py
@@ -0,0 +1,13 @@
+print ("Please enter your first name")
+
+f_name = input ("First Name:")
+
+print ("Please enter your last name")
+
+l_name = input ("Last Name:")
+
+print (f"Hello {f_name} {l_name}")
+
+age = input ("Proceed to enter your age:")
+
+print (f"Hello {f_name} {l_name}. Your age is {age}")
